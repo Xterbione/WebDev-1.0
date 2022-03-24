10a50c867d13dd8b62934abae05282f337d5df73
xterbione <bryan_K_@hotmail.com>
2022-03-24 18:12:41 +0100

    added drukmanager.

--- WebDevProject/WebDevProject/Repositories/DrukRepo.cs
+++ WebDevProject/WebDevProject/Repositories/DrukRepo.cs
@@ -79,5 +79,76 @@ namespace WebDevProject.Repositories
                         }, new {search}, splitOn: "stripboekId, serieId, genreId, uitgeverId");
             return strips;
         }
+
+        /// <summary>
+        /// inserting new druk
+        /// </summary>
+        /// <param name="stripboekid"></param>
+        /// <param name="druknummer"></param>
+        /// <param name="drukdatum"></param>
+        /// <param name="uitvoering"></param>
+        /// <param name="oplage"></param>
+        /// <param name="waarde"></param>
+        /// <param name="isbn"></param>
+        /// <param name="uitgeverid"></param>
+        public void insert(int stripboekid, int druknummer, DateTime drukdatum,string uitvoering, int oplage, double waarde, int isbn,int uitgeverid)
+        {
+            using var connection = GetConnection();
+            var sql = @"
+                INSERT INTO druk (stripboek_id, druknummer, druk_datum, uitvoering, oplage, waarde, isbn, uitgever_id) 
+                VALUES (@stripboekid, @druknummer, @drukdatum, @uitvoering, @oplage, @waarde, @isbn, @uitgeverid );";
+            var removeSeparate = connection.Execute(sql, new { stripboekid, druknummer, drukdatum, uitvoering, oplage, waarde, isbn, uitgeverid });
+        }
+
+
+        /// <summary>
+        /// updating existing druk
+        /// </summary>
+        /// <param name="stripboekid"></param>
+        /// <param name="druknummer"></param>
+        /// <param name="drukdatum"></param>
+        /// <param name="uitvoering"></param>
+        /// <param name="oplage"></param>
+        /// <param name="waarde"></param>
+        /// <param name="isbn"></param>
+        /// <param name="uitgeverid"></param>
+        /// <param name="drukid">the id of the druk you want to edit</param>
+        public void update(int stripboekid, int druknummer, DateTime drukdatum, string uitvoering, int oplage, double waarde, int isbn, int uitgeverid, int drukid)
+        {
+            using var connection = GetConnection();
+            var sql =
+                @"update druk set stripboek_id = @stripboekid, druknummer = @druknummer, druk_datum = @drukdatum, uitvoering = @uitvoering, oplage = @oplage, waarde = @waarde, isbn = @isbn, uitgever_id = @uitgeverid WHERE drukId = @drukid";
+            var removeSeparate = connection.Execute(sql, new { stripboekid, druknummer, drukdatum, uitvoering, oplage, waarde, isbn, uitgeverid, drukid });
+        }
+
+        /// <summary>
+        /// returns number of druk with a specific isbn
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns></returns>
+        public int count(int isbn)
+        {
+            string sql = "SELECT COUNT(drukId) FROM druk where isbn=@isbn";
+            using var connection = GetConnection();
+            int count = connection.ExecuteScalar<int>(sql, new { isbn });
+            return count;
+
+        }
+
+        /// <summary>
+        /// deletes a single druk from the db based on its id. also deletes associating relational entries
+        /// </summary>
+        /// <param name="drukid"></param>
+        public void DeleteSingle(int drukid)
+        {
+            WerktAanRepo werktAanRepo = new WerktAanRepo();
+            werktAanRepo.DeleteMultiple(drukid);
+            //the query
+            string sql = @"DELETE FROM druk WHERE drukId = @drukid";
+            //the connection
+            using var connection = GetConnection();
+            //executes query
+            connection.Execute(sql, new { drukid });
+        }
     }
 }
