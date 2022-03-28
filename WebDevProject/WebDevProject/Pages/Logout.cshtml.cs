d91e9af8ccba0acc2ba1813f71ffe9e0d4431b69
AllardArcade <allard.de.groot@student.nhlstenden.com>
2022-03-28 12:19:13 +0200

    commit

--- WebDevProject/WebDevProject/Pages/Logout.cshtml.cs
+++ WebDevProject/WebDevProject/Pages/Logout.cshtml.cs
@@ -5,14 +5,18 @@ namespace WebDevProject.Pages
 {
     public class LogoutModel : PageModel
     {
-       // public string Message { get; set; }
-        
-
-        public IActionResult OnPost()
-        {
-            HttpContext.Session.Remove("cockie");
-            return new RedirectToPageResult("/Index");
+        public IActionResult OnGet()
+        { 
+            // SessionExten
+            if (HttpContext.Session.GetString("cockie") != null)
+            {
+                HttpContext.Session.Remove("cockie");
+              
+                return new RedirectToPageResult("/Index");
+            }
+            // return new RedirectToPageResult("/Index");
+            return Page();
+           
         }
-
     }
 }
